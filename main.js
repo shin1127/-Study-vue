0cd27e073f0bd5c0ee7cee9f3c64669ec116d141
shin1127 <ellendis1124@gmail.com>
2021-01-07 23:20:07 +0900

    f

---
+++ main.js
@@ -0,0 +1,14 @@
+// var app = new Vue({
+//     el: '#app'
+// })
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     window.app = new Vue()
+// })
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
